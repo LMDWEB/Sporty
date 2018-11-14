d45cae8bb4445fe99c39f7450e449955717fb985
Kevin <kev_kevin@hotmail.fr>
2018-11-14 12:40:06 +0100

    [FEATURE] authentification partie2 (login form)

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -24,8 +24,9 @@ class UserFixtures extends Fixture
         $user = new User();
         $user->setFirstName('test')
              ->setLastName('test')
+             ->setUsername('test')
              ->setEmail('test@test.fr')
-             ->setPassword($this->encoder($user, 'password'));
+             ->setPassword($this->encoder->encodePassword($user, 'password'));
 
         $manager->persist($user);
         $manager->flush();
