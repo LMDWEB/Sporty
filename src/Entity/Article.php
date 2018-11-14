4b4644022b212ef5a0455d6f4a5f12be39a9c904
Diego <waziri.diego@gmail.com>
2018-11-14 12:46:10 +0100

    [FIX] clean article

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -52,24 +52,11 @@ class Article
 	private $content = "";
 
     /**
-     * @var User
-     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="articles")
-     */
-    private $user;
-
-    /**
      * @var \DateTime
      * @ORM\Column(type="datetime")
      * @Assert\Type(type="\DateTime")
      */
-
     private $date;
-
-    /**
-     * @ORM\ManyToMany(targetEntity="App\Entity\Player", inversedBy="articles")
-     *
-     */
-    private $player;
 
     /**
      * @ORM\Column(type="text")
@@ -100,24 +87,6 @@ class Article
      * @ORM\Column(type="string", length=30, nullable=true)
      */
     private $source_image;
-
-    /**
-     * @ORM\ManyToMany(targetEntity="App\Entity\Thread", mappedBy="article")
-     */
-    private $threads;
-
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="article")
-     */
-    private $comments;
-    
-
-    public function __construct()
-    {
-        $this->player = new ArrayCollection();
-        $this->threads = new ArrayCollection();
-        $this->comments = new ArrayCollection();
-    }
 
     /**
      * @return int
@@ -202,32 +171,6 @@ class Article
         return $this;
     }
 
-    /**
-     * @return Collection|Player[]
-     */
-    public function getPlayer(): Collection
-    {
-        return $this->player;
-    }
-
-    public function addPlayer(Player $player): self
-    {
-        if (!$this->player->contains($player)) {
-            $this->player[] = $player;
-        }
-
-        return $this;
-    }
-
-    public function removePlayer(Player $player): self
-    {
-        if ($this->player->contains($player)) {
-            $this->player->removeElement($player);
-        }
-
-        return $this;
-    }
-
     public function getImage(): ?string
     {
         return $this->image;
@@ -296,65 +239,6 @@ class Article
     public function setSourceImage(?string $source_image): self
     {
         $this->source_image = $source_image;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Thread[]
-     */
-    public function getThreads(): Collection
-    {
-        return $this->threads;
-    }
-
-    public function addThread(Thread $thread): self
-    {
-        if (!$this->threads->contains($thread)) {
-            $this->threads[] = $thread;
-            $thread->addArticle($this);
-        }
-
-        return $this;
-    }
-
-    public function removeThread(Thread $thread): self
-    {
-        if ($this->threads->contains($thread)) {
-            $this->threads->removeElement($thread);
-            $thread->removeArticle($this);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Comment[]
-     */
-    public function getComments(): Collection
-    {
-        return $this->comments;
-    }
-
-    public function addComment(Comment $comment): self
-    {
-        if (!$this->comments->contains($comment)) {
-            $this->comments[] = $comment;
-            $comment->setArticle($this);
-        }
-
-        return $this;
-    }
-
-    public function removeComment(Comment $comment): self
-    {
-        if ($this->comments->contains($comment)) {
-            $this->comments->removeElement($comment);
-            // set the owning side to null (unless already changed)
-            if ($comment->getArticle() === $this) {
-                $comment->setArticle(null);
-            }
-        }
 
         return $this;
     }
