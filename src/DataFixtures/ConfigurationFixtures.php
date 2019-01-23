704957d86cc3b62b6ca551a7fa7f2979b5bffb86
Diego <waziri.diego@gmail.com>
2019-01-23 19:21:10 +0100

    [FIX] add datafixture for config website

--- src/DataFixtures/ConfigurationFixtures.php
+++ src/DataFixtures/ConfigurationFixtures.php
@@ -11,11 +11,50 @@ class ConfigurationFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
 
-        $config = new Configuration();
-        $config->setNameConfig('name_site')
-            ->setValueConfig('Le Meilleur du PSG')
-            ->setType(0);
-        $manager->persist($config);
+        $configs = array(
+            array(
+                "name" => "namesite",
+                "value" => "",
+                "type" => 0,
+
+            ),
+            array(
+                "name" => "description",
+                "value" => "",
+                "type" => 0,
+
+            ),
+            array(
+                "name" => "facebook",
+                "value" => "facebook.com/",
+                "type" => 1,
+
+            ),
+            array(
+                "name" => "twitter",
+                "value" => "twitter.com/",
+                "type" => 1,
+
+            ),
+            array(
+                "name" => "instagram",
+                "value" => "instagram.com/",
+                "type" => 1,
+
+            ),
+        );
+
+        foreach ($configs as  $config) {
+            $configuration = new Configuration();
+            $configuration
+                ->setNameConfig($config['name'])
+                ->setValueConfig($config['value'])
+                ->setType($config['type']);
+            ;
+
+            $manager->persist($configuration);
+        }
+
 
         $manager->flush();
     }
