e093db854a4099813a73da0b706b7c990299849d
Anton Bourtnik <anton.bourtnik@hotmail.fr>
2019-01-24 16:58:17 +0100

    Mise en forme du CRUD user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Role;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,10 +15,22 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('username')
-            ->add('email')
+            ->add('firstname', TextType::class, array(
+                'label' => 'user.firstname',
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('lastName', TextType::class, array(
+                'label' => 'user.lastname',
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('username', TextType::class, array(
+                'label' => 'user.username',
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('email', TextType::class, array(
+                'label' => 'user.email',
+                'attr' => array('class' => 'form-control')
+            ))
            /* ->add('roles', EntityType::class, [
                 'label'        => 'Role',
                 'class'        => Role::class,
