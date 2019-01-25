f50deb0f1ae5608317e8bed0db4d67ea5a0c85b2
Diego <waziri.diego@gmail.com>
2019-01-25 18:59:26 +0100

    [FIX] edit file temporarily

--- src/Controller/Front/CategoryController.php
+++ src/Controller/Front/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Front;
 
 use App\Entity\Article;
 use App\Entity\Category;
+use App\Service\PaginationService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,18 +16,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/{slug}", name="category", methods="GET")
+     * @Route("/{slug}/{page<\d+>?1}", name="category", methods="GET")
      */
 
-    public function index(Category $category, Request $request): Response
+    public function index(Category $category, Request $request, $page, PaginationService $pagination): Response
     {
+
         $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
+
+        $pagination->setEntityClass(Article::class)->getData(['published' => 1, "category" => $category, 'date' => 'DESC']);
+        $pagination->setCurrentPage($page);
+        $pagination->setLimit(10);
+
         $allArticles = $this->getDoctrine()->getRepository(Article::class)->findBy(['published' => 1, 'type' => 0], ['date' => 'DESC'], 10, 0);
-        $articles = $this->getDoctrine()->getRepository(Article::class)->findBy(['published' => 1, "category" => $category], ['date' => 'DESC']);
 
         return $this->render('Front/category/index.html.twig', [
             'category' => $category,
-            'articles' => $articles,
+            'articles' => $pagination,
             'categories' => $categories,
             'allArticles' => $allArticles,
         ]);
